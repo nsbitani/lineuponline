72cd32e08d094fe933fa045af6ef90f088a28bc2
nsbitani <nsbitani@gmail.com>
2016-08-27 10:42:20 -0400

    add question demo

---
+++ index.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+        $("#no").click(function(){
+            $("#help").removeClass('hidden');
+            $(".yn").addClass('hidden');
+        });
+        $("#yes").click(function(){
+            $(".yn").addClass('hidden');
+            $("#question").removeClass('hidden');
+        });
+        $('#office li a').on('click', function(){
+            $("#boo").addClass('hidden');
+            $(".person").addClass('hidden');
+            var val = this.innerHTML;
+            $('#'+val).removeClass('hidden');
+            $("#yesnow").removeClass('hidden');
+        });
+        $("#yesnow").click(function(){
+            $("#help").addClass('hidden');
+            $(".person").addClass('hidden');
+            $("#yesnow").addClass('hidden');
+            $("#question").removeClass('hidden');
+        });
+
+        $(".correct").click(function(){
+            $("#question").addClass('hidden');
+            $("#yay").removeClass('hidden');
+        });
+        $(".incorrect").click(function(){
+            $("#question").addClass('hidden');
+            $("#boo").removeClass('hidden');
+            $("#help").removeClass('hidden');
+        });
+        $("#next").click(function(){
+            $("#yay").addClass('hidden');
+            $("#lineup").removeClass('hidden');
+        });
+    });
